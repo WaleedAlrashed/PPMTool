2b55b2acbfa7f0a424f06c7e091ddff20d9a8568
Waleed Alrashed <wmr1210@hotmail.com>
2019-05-02 23:04:51 +0300

    second commit

---
+++ ppmt/ppmtool/src/test/java/com/sytech/ppmtl/PpmtlApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sytech.ppmtl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PpmtlApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
